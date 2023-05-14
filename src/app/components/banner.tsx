3450dd6c3f6f08d17854a09a3d845a4fa204aa8a
Senlima <senlima0430@gmail.com>
2023-05-15 07:52:14 +0800

    chore: update

--- src/app/components/banner.tsx
+++ src/app/components/banner.tsx
@@ -5,7 +5,7 @@ const Banner: React.FC = () => {
 		<header className="relative mx-auto w-[90%] max-w-[765px] rounded-lg flex flex-col overflow-hidden">
 			<section className="mb-8 text-center">
 				<h2 className="text-xl md:text-3xl font-semibold">Mesh Sun</h2>
-				<h1 className="text-sm md:text-md animate-softFadeIn">Front-end developer | Former Binancian | 20 yrs old</h1>
+				<h1 className="text-sm md:text-md animate-softFadeIn">Front-end developer | Former Binancian | 21 yrs old</h1>
 			</section>
 			<img width={765} height={104} src="/wave.svg" alt="decorator" />
 		</header>
